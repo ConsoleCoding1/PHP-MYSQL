beb6202d843a7ad5ba8fc70b013ad1a5d3b678ab
Krish <krishkrsah10@gmail.com>
2025-10-25 14:34:29 +0530

    Add 'Sections' link to header and update section.php for student insertion

--- header.php
+++ header.php
@@ -8,6 +8,7 @@
                 <a href="class.php" class="ml-15 text-3xl text-white hover:text-red-500 transition duration-500 corsor-potinter">Class</a>
                 <a href="teachers.php" class="ml-15 text-3xl text-white hover:text-red-500 transition duration-500 corsor-potinter">Teachers</a>
                 <a href="students.php" class="ml-15 text-3xl text-white hover:text-red-500 transition duration-500 corsor-potinter">Students</a>
+                <a href="section.php" class="ml-15 text-3xl text-white hover:text-red-500 transition duration-500 corsor-potinter">Sections</a>
             </div>
             <div></div>
         </div>
--- section.php
+++ section.php
@@ -1,45 +1,42 @@
+<?php
+include("./config.php");
+if (isset($_REQUEST['submit'])) {
+    $section = $_REQUEST["section"];
+    $sqlQuery = "INSERT INTO schoolstudents (`roll_no`, `first_name`,`last_name`,`MobNo`,`class`) VALUES (NULL, 'Krish', 'Kuamar Sah', '919381390520', '9')";
+    $student = $conn->prepare($sqlQuery);
+    $result = $student->execute();
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Section</title>
 </head>
+
 <body>
     <?php include('./header.php') ?>
-    <h1 class="text-center text-3xl">Students</h1>
+    <h1 class="text-center text-3xl">Section</h1>
     <div class="flex justify-center my-5">
         <table class="border border-gray-400 border-collapse px-2 mr-10">
             <tr>
-                <th class="border border-gray-400 border-collapse px-2 py-2"></th>
+                <th class="border border-gray-400 border-collapse px-2 py-2">Sections</th>
             </tr>
-            <?php
-            $studentsList = $conn->prepare("SELECT * FROM `schoolstudents`");
-            $studentsList->execute();
-            $result2 = $studentsList->fetchAll();
-            foreach ($result2 as $teacher) {
-
-            ?>
-                <tr>
-                    <td class="border border-gray-400 border-collapse px-2 py-2"><?= $teacher['roll_no'] ?></td>
-                    <td class="border border-gray-400 border-collapse px-2 py-2"><?= $teacher['first_name'] ?></td>
-                    <td class="border border-gray-400 border-collapse px-2 py-2"><?= $teacher['last_name'] ?></td>
-                    <td class="border border-gray-400 border-collapse px-2 py-2"><?= $teacher['MobNo'] ?></td>
-                    <td class="border border-gray-400 border-collapse px-2 py-2"><?= $teacher['class'] ?></td>
-                    						
-
-                </tr>
-            <?php
-            }
-            ?>
-        </table> 
-        <div class="max-w-1/2 max-sm:max-w-lg justify-center">
+            <tr>
+                <td class="border border-gray-400 border-collapse px-2 py-2">a</td>
+            </tr>
+        </table>
+        <div class="">
 
             <form method="post" action="students.php">
                 <div class="grid sm:grid-cols-2 gap-8">
                     <div>
                         <label class="text-slate-900 text-xl font-medium mb-2 block">Enter Secion</label>
-                        <input name="name" type="text" class="bg-slate-100 w-full text-slate-900 text-xl px-4 py-3 rounded-md focus:bg-transparent outline-blue-500 transition-all" placeholder="Enter Section" />
+                        <input name="section" type="text" class="bg-slate-100 w-full text-slate-900 text-xl px-4 py-3 rounded-md focus:bg-transparent outline-blue-500 transition-all" placeholder="Enter Section" />
                     </div>
                 </div>
 
@@ -53,4 +50,5 @@
     </div>
     <?php include('./footer.php') ?>
 </body>
+
 </html>
