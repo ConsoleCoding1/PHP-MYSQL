881795cd705473eb7ea1c4a372f09f7ecc218301
Krish <krishkrsah10@gmail.com>
2025-10-25 14:34:58 +0530

    Refactor section.php to insert new sections and display them in a table

--- section.php
+++ section.php
@@ -2,9 +2,9 @@
 include("./config.php");
 if (isset($_REQUEST['submit'])) {
     $section = $_REQUEST["section"];
-    $sqlQuery = "INSERT INTO schoolstudents (`roll_no`, `first_name`,`last_name`,`MobNo`,`class`) VALUES (NULL, 'Krish', 'Kuamar Sah', '919381390520', '9')";
-    $student = $conn->prepare($sqlQuery);
-    $result = $student->execute();
+    $sqlQuery = "INSERT INTO section (`id`, `section`) VALUES (NULL, '$section')";
+    $section = $conn->prepare($sqlQuery);
+    $result = $section->execute();
 }
 
 ?>
@@ -26,27 +26,35 @@ if (isset($_REQUEST['submit'])) {
             <tr>
                 <th class="border border-gray-400 border-collapse px-2 py-2">Sections</th>
             </tr>
-            <tr>
-                <td class="border border-gray-400 border-collapse px-2 py-2">a</td>
-            </tr>
+            <?php
+            $sectionList = $conn->prepare("SELECT * FROM `section`");
+            $sectionList->execute();
+            $result2 = $sectionList->fetchAll();
+            foreach ($result2 as $section) {
+
+            ?>
+                <tr>
+                    <td class="border border-gray-400 border-collapse px-2 py-2"><?= $section['section'] ?></td>
+                </tr>
+            <?php
+            }
+
+            ?>
         </table>
-        <div class="">
-
-            <form method="post" action="students.php">
-                <div class="grid sm:grid-cols-2 gap-8">
-                    <div>
-                        <label class="text-slate-900 text-xl font-medium mb-2 block">Enter Secion</label>
-                        <input name="section" type="text" class="bg-slate-100 w-full text-slate-900 text-xl px-4 py-3 rounded-md focus:bg-transparent outline-blue-500 transition-all" placeholder="Enter Section" />
-                    </div>
+        <form method="post" action="section.php">
+            <div class="gap-8">
+                <div>
+                    <label class="text-slate-900 text-xl font-medium mb-2 block">Enter Secion</label>
+                    <input name="section" type="text" class="bg-slate-100 w-full text-slate-900 text-xl px-4 py-3 rounded-md focus:bg-transparent outline-blue-500 transition-all" placeholder="Enter Section" />
                 </div>
+            </div>
 
-                <div class="mt-12">
-                    <button name="submit" type="submit" class="mx-auto block min-w-32 py-3 px-6 text-xl font-medium tracking-wider rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none cursor-pointer">
-                        Register
-                    </button>
-                </div>
-            </form>
-        </div>
+            <div class="mt-12">
+                <button name="submit" type="submit" class=" block min-w-32 py-3 px-6 text-xl font-medium tracking-wider rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none cursor-pointer">
+                    Register
+                </button>
+            </div>
+        </form>
     </div>
     <?php include('./footer.php') ?>
 </body>
